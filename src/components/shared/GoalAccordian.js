0d04008add8aa5fc8b73f235b53c28b6ee685f98
Qianru69 <qwei5@dons.usfca.edu>
2023-04-01 21:26:46 -0700

    Changed the reflection form

--- src/components/shared/GoalAccordian.js
+++ src/components/shared/GoalAccordian.js
@@ -30,7 +30,7 @@ import DoneIcon from '@mui/icons-material/Done';
 function generate(element) {
     return [0, 1, 2].map((value) =>
         React.cloneElement(element, {
-            key: value,git
+            key: value,
         }),
     );
 }
